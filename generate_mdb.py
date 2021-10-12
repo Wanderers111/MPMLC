47df2fcf3d09fc05290bb77eb0235482bdb0cf4c
showmaker <bowen.zhang@xtalpi.com>
2021-10-12 21:46:41 +0800

    update the mol.py, conformation generation

--- generate_mdb.py
+++ generate_mdb.py
@@ -2,7 +2,9 @@ from api.generate_conformation_encode import generate_and_encode
 import pickle
 import pandas as pd
 from multiprocessing.pool import Pool
+import time
 
+time1 = time.time()
 table = pd.read_csv('./zinc15_druglike_clean_canonical_max60.smi', header=None)
 smis = table[0].values.tolist()
 processing = Pool(10, maxtasksperchild=1000)
@@ -14,3 +16,4 @@ processing.join()
 zlibs_final_list = [zlib_i.get() for zlib_i in zlibs_list]
 with open('dataset.mdb', 'wb') as f:
     pickle.dump(zlibs_final_list, f)
+print(time.time()-time1)
