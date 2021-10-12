e7f473df70d3e32cca1d21e797a30f9c93d56c4e
showmaker <bowen.zhang@xtalpi.com>
2021-10-12 21:45:14 +0800

    update the mol.py, conformation generation

--- api/generate_conformation_encode.py
+++ api/generate_conformation_encode.py
@@ -1,10 +1,9 @@
-from multiprocessing import Pool
+import zlib
+
 from rdkit import Chem
 from rdkit.Chem.PropertyMol import PropertyMol
-from modules.mol import conformation_generation, get_mol_coordinate, get_mol_type_one_hot
-import zlib
-import pandas as pd
-import pickle
+
+from modules.mol import conformation_generation
 
 
 def generate_and_encode(smi):
@@ -27,17 +26,3 @@ def generate_and_encode(smi):
     del mol
     del mol_blocks_list
     return zlib_s
-
-
-if __name__ == '__main__':
-    table = pd.read_csv('./zinc15_druglike_clean_canonical_max60.smi', header=None)
-    smis = table[0].loc[:100000].values.tolist()
-    processing = Pool(10, maxtasksperchild=1000)
-    zlibs_list = []
-    for smi_i in smis:
-        zlibs_list.append(processing.apply_async(generate_and_encode, args=(smi_i,)))
-    processing.close()
-    processing.join()
-    zlibs_final_list = [zlib_i.get() for zlib_i in zlibs_list]
-    with open('dataset.mdb', 'wb') as f:
-        pickle.dump(zlibs_final_list, f)
---
+++ generate_mdb.py
@@ -0,0 +1,16 @@
+from api.generate_conformation_encode import generate_and_encode
+import pickle
+import pandas as pd
+from multiprocessing.pool import Pool
+
+table = pd.read_csv('./zinc15_druglike_clean_canonical_max60.smi', header=None)
+smis = table[0].values.tolist()
+processing = Pool(10, maxtasksperchild=1000)
+zlibs_list = []
+for smi_i in smis:
+    zlibs_list.append(processing.apply_async(generate_and_encode, args=(smi_i,)))
+processing.close()
+processing.join()
+zlibs_final_list = [zlib_i.get() for zlib_i in zlibs_list]
+with open('dataset.mdb', 'wb') as f:
+    pickle.dump(zlibs_final_list, f)
